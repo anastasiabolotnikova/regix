74c362ebeb88572a2a0c7dde0eef3e6108616e90
anastasiabolotnikova <bolotnikova.anastasia@gmail.com>
2014-03-29 18:01:13 +0200

    Documentation, event registration views

--- backend/models/RegistrationPageModel.php
+++ backend/models/RegistrationPageModel.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * @file RegistrationPageModel.php
+ * 
+ * This file contains registration page model class.
+ */
+
+/**
+ * This file extends Model class.
+ */
 require_once REGIX_PATH.'models/Model.php';
 
 try {
@@ -10,8 +19,17 @@ try {
 
 class RegistrationPageModel extends Model{
 	
-	protected $local_login_model;
-	
+	/**
+	 * Checks validity of the data entered by a client into the registration form.
+	 *
+	 * @param unknown $name user's name.
+	 * @param unknown $login username.
+	 * @param unknown $password user's password.
+	 * @param unknown $repassword retyped password.
+	 * @param unknown $email user's email.
+	 *
+	 * @return bool TRUE if all the data entered by a client is valid, FALSE otherwise.
+	 */
 	public function plaintextCheck(
 			$name,
 			$login,
@@ -62,7 +80,16 @@ class RegistrationPageModel extends Model{
 		
 		return TRUE;
 	}
-	
+	/**
+	 * Saves data submited by a client to the database.
+	 * Generates salt and hash for the password.
+	 *
+	 * @param unknown $name user's name.
+	 * @param unknown $login username.
+	 * @param unknown $password user's password.
+	 * @param unknown $email user's email.
+	 *
+	 */
 	public function save_data($name, $login, $password, $email) {
 		
 		$salt = LocalLoginModel::gen_salt("SHA512");
