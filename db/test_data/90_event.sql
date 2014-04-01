0598817ac541cc7429068cee1fcf0217876103c7
myloginistaken <prokopov@hotmail.com>
2014-04-01 21:07:34 +0300

    Event table fixed

--- db/test_data/90_event.sql
+++ db/test_data/90_event.sql
@@ -4,11 +4,7 @@ LIMIT 0, 1000
 
 -- Date: 2014-03-21 20:10
 */
-INSERT INTO `event`(`id`, `calendar_id`, `name`, `description`, `assigned_user`, `from`, `to`) VALUES (1,1,"Test Event","Test event",4,TIMESTAMP '2014-3-12 8:00:00',TIMESTAMP '2014-3-12 9:00:00');
-INSERT INTO `event`(`id`, `calendar_id`, `name`, `description`, `assigned_user`, `from`, `to`) VALUES (2,1,"Test Event2","Test event",5,TIMESTAMP '2014-3-12 10:00:00',TIMESTAMP '2014-3-12 11:00:00');
-INSERT INTO `event`(`id`, `calendar_id`, `name`, `description`, `assigned_user`, `from`, `to`) VALUES (3,1,"Test Event3","Test event",6,TIMESTAMP '2014-3-12 11:00:00',TIMESTAMP '2014-3-12 12:00:00');
-INSERT INTO `event`(`id`, `calendar_id`, `name`, `description`, `assigned_user`, `from`, `to`) VALUES (4,1,"Test Event4","Test event",4,TIMESTAMP '2014-3-12 15:00:00',TIMESTAMP '2014-3-12 16:00:00');
-INSERT INTO `event`(`id`, `calendar_id`, `name`, `description`, `assigned_user`, `from`, `to`) VALUES (5,1,"Test Event5","Test event",5,TIMESTAMP '2014-4-12 11:00:00',TIMESTAMP '2014-4-12 12:00:00');
-INSERT INTO `event`(`id`, `calendar_id`, `name`, `description`, `assigned_user`, `from`, `to`) VALUES (6,1,"Test Event6","Test event",6,TIMESTAMP '2014-4-12 13:00:00',TIMESTAMP '2014-4-12 14:00:00');
-INSERT INTO `event`(`id`, `calendar_id`, `name`, `description`, `assigned_user`, `from`, `to`) VALUES (7,1,"Test Event7","Test event",4,TIMESTAMP '2014-4-12 16:00:00',TIMESTAMP '2014-4-12 17:00:00');
-INSERT INTO `event`(`id`, `calendar_id`, `name`, `description`, `assigned_user`, `from`, `to`) VALUES (8,1,"Test Event8","Test event",5,TIMESTAMP '2014-4-12 17:00:00',TIMESTAMP '2014-4-12 18:00:00');
+INSERT INTO `event`(`calendar_id`, `user_id`, `description`, `assigned_user`, `assigned_service`, `from`, `to`) VALUES (1,2,'I have a problem',4,'tserv1', TIMESTAMP '2014-3-12 8:00:00',TIMESTAMP '2014-3-12 9:00:00');
+INSERT INTO `event`(`calendar_id`, `user_id`, `description`, `assigned_user`, `assigned_service`, `from`, `to`) VALUES (1,3,'I have a problem',5,'tserv2', TIMESTAMP '2014-3-12 10:00:00',TIMESTAMP '2014-3-12 11:00:00');
+INSERT INTO `event`(`calendar_id`, `user_id`, `description`, `assigned_user`, `assigned_service`, `from`, `to`) VALUES (1,2,'I have a problem',6,'tserv3', TIMESTAMP '2014-3-12 11:00:00',TIMESTAMP '2014-3-12 12:00:00');
+INSERT INTO `event`(`calendar_id`, `user_id`, `description`, `assigned_user`, `assigned_service`, `from`, `to`) VALUES (1,3,'I have a problem',4,'tserv1',TIMESTAMP '2014-3-12 15:00:00',TIMESTAMP '2014-3-12 16:00:00');
