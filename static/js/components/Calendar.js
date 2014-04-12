2e59f509fb2e3b0bdef9c435c684ad162c4b3ff4
sergeijakovlev <sergei.jakovlev.10@gmail.com>
2014-04-12 10:18:55 +0300

    [ajax] Objects.

--- static/js/components/Calendar.js
+++ static/js/components/Calendar.js
@@ -1,76 +1,102 @@
-var old_ids = [];
-var first = true;
+// Event
 
-function informAboutNewEvents(new_ids, new_descriptions, times_f, times_t) {
-	
-	popup = $("#popup_top_tpl").clone();
-	popup.removeAttr('id');
-	popup.empty();
-	event_tpl = $("#popup_top_tpl .event");
-	
-	var needToShow = false;
-	
-	for (i = 0; i < new_ids.length; ++i) {
-
-		if($.inArray(new_ids[i], old_ids) == -1) {
-			old_ids.push(new_ids[i]);
-			
-			var event_cur = event_tpl.clone();
-			$(event_cur).find(".time").html(times_f[i]+" — "+times_t[i]);
-			$(event_cur).find(".description").html(new_descriptions[i]);
-			popup.append(event_cur);
-			
-			needToShow = true;
-		}
-	}
-	
-	if (first) {
-		first = false;
-		return;
-	}
-	
-	if(needToShow) {
-		$("#header").append(popup);
-		popup.show("slow");
-		
-		new function(popup) {
-			setTimeout(function () {
-				popup.hide("slow");
-			}, 5000);
-		}(popup);
-		
-	}
+function Event(id, from, to, user, description) {
+	this.id = id;
+	this.from = from;
+	this.to = to;
+	this.user = user;
+	this.description = description;
 }
 
-function getLatestEvents() {
+Event.parseEvent = function(ev_data) {
+	return new Event(
+			$(ev_data).find("id").text(),
+			$(ev_data).find("from").text(),
+			$(ev_data).find("to").text(),
+			"TEST",
+			$(ev_data).find("description").text()
+			);
+}
+
+
+// Update manager
+
+function UpdateManager(url) {
+	this.url = url;
+	
+	this.events = {};
+	
+	this.callbacks = $.Callbacks();
+}
+
+UpdateManager.prototype.addCallback = function(callbacks) {
+	this.callbacks.add(callbacks);
+}
+
+UpdateManager.prototype.removeCallback = function(callbacks) {
+	this.callbacks.remove(callbacks);
+}
+
+UpdateManager.prototype.parseData = function(data, update_manager) {
+	console.log(update_manager);
+	
+	xmlData = $($.parseXML(data));
+	
+	xmlData.find("event").each(
+			function (idx, value) {
+				var event = Event.parseEvent(value);
+				if (!update_manager.events[event.id]) {
+					update_manager.events[event.id] = event;
+					update_manager.callbacks.fire(event);
+				}
+			});
+}
+
+UpdateManager.prototype.update = function(url) {
+	var self = this;
 	$.ajax({
-		url: '/latest/events',
+		url: this.url,
 		success: function(data) {
-			xmlEvDoc = $.parseXML(data);
-			xmlEv = $(xmlEvDoc);
-			
-			var ids = [];
-			var descriptions = [];
-			var times_f = [];
-			var times_t = [];
-			
-			xmlEv.find("event").each(
-					function (idx, value) {
-						ids.push($(value).find("id").text());
-						descriptions.push($(value).find("description").text());
-						times_f.push($(value).find("from").text());
-						times_t.push($(value).find("to").text());
-					});
-			
-			informAboutNewEvents(ids, descriptions, times_f, times_t);
-			
+			self.parseData(data, self);
 		}
 	});
 }
 
-var updater = function ($timeout) {
-	getLatestEvents();
-	setTimeout(function () {
-		updater($timeout);
-	}, $timeout);
-};
+
+// UI
+
+function SlottedUI(from, to, slot_count) {
+	this.from = from;
+	this.to = to;
+	this.slot_count = slot_count;
+	this.slot_size = (to - from) / slot_count;
+	
+	this.timeline = $("#timeline");
+	this.timeline_slot_tpl = $("#timeline_slot_tpl").clone().removeAttr("id");
+	this.current_time_line = $("#current_time_line");
+	
+	this.slots = this.generate_slots(slot_count);
+}
+
+SlottedUI.generate_slots(slot_count) {
+	for (var int = 0; int < slot_count; int++) {
+		
+	}
+}
+
+SlottedUI.prototype.eventAdder = function(event, ui) {
+	console.log(this);
+	var cur_timeslot = this.timeline_slot_tpl.clone();
+	cur_timeslot.find(".timeslot_from").text(event.from);
+	cur_timeslot.find(".timeslot_to").text(event.to);
+	
+	this.timeline.append(cur_timeslot);
+}
+
+var url = '/latest/events';
+
+sui = new SlottedUI(480, 1080, 10)
+um = new UpdateManager(url);
+um.addCallback(function(event) {
+	sui.eventAdder(event, sui);
+});
