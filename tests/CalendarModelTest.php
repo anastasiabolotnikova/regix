5f968247f4bc363f53bdce40a7b681c08ca55b21
sergeijakovlev <sergei.jakovlev.10@gmail.com>
2014-04-13 17:35:37 +0300

    [tests] Minor changes to CalendarModelTest. Add phpUnit config file.

--- tests/CalendarModelTest.php
+++ tests/CalendarModelTest.php
@@ -1,13 +1,44 @@
 <?php
 define("REGIX_PATH", "backend/");
 
+require_once REGIX_PATH.'core/Config.php';
+
+require_once REGIX_PATH."core/adapters/MySQL_Adapter.php";
 require_once REGIX_PATH.'models/CalendarModel.php';
 require_once REGIX_PATH.'models/Model.php';
 
+
+class UserStub {
+	public function get_id() {
+		return 1;
+	}
+}
+
+class SessionStub {
+	public $user;
+	
+	public function SessionStub() {
+		$this->user = new UserStub();
+	}
+}
+
 class CalendarModelTest extends PHPUnit_Framework_TestCase{
 	
-	public function test_get_current_date() {
+	private $config;
+	private $db;
+	private $session;
+	
+	public function __construct() {
+		parent::__construct();
 		
+		$this->config = new Config(REGIX_PATH."config/regix.ini");
+		$this->db = new MySQL_Adapter($this->config);
+		$this->session = new SessionStub();
+		
+		$this->db->connect();
+	}
+	
+	public function test_get_current_date() {
 		$day = date('j');
 		$month = date('n');
 		$year = date('o');
@@ -19,7 +50,7 @@ class CalendarModelTest extends PHPUnit_Framework_TestCase{
 		$this->assertEquals($model->get_day(), $day);
 		$this->assertEquals($model->get_month(), $month);
 		$this->assertEquals($model->get_year(), $year);
-		$this->assertEquals($model->get_wd(), $wd);
+		$this->assertEquals($model->get_wd($month, $year), $wd);
 	}
 	
 	public function test_save_data(){
@@ -27,12 +58,12 @@ class CalendarModelTest extends PHPUnit_Framework_TestCase{
 		$day = date('j');
 		$month = date('n');
 		$year = date('o');
-		$wd = date('w', mktime(0,0,0,$this->month,1,$year));
+		$wd = date('w', mktime(0,0,0,$month,1,$year));
 		$model = new CalendarModel($this->db, $this->session);
 		
 		$description = 'Description by auto test';
 		//$worker_ar = $model->$this->db->select_user(5);
-		$worker = 'Tset Worker 2';//$worker_ar[0]['name'];
+		$worker = 'Test Worker 2';//$worker_ar[0]['name'];
 		$assigned_user = 5;
 		$time_start = 8;
 		$assigned_service = 'tserv1';
@@ -43,8 +74,8 @@ class CalendarModelTest extends PHPUnit_Framework_TestCase{
 		
 		$model->save_data($year, $month, $day, $description, $assigned_user, $assigned_service, $time_start);
 		
-		$last_id = $model->$this->db->get_last_id();
-		$event_data = $model->$this->db->select_event_data($last_id);
+		$last_id = $this->db->get_last_id();
+		$event_data = $this->db->select_event_data($last_id);
 		$event = $event_data[0];
 		
 		$this->assertEquals($event['from'], $from);
