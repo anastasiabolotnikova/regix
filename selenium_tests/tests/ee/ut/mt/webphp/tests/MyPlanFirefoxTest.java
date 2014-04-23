0f2d5c8b33ffcd6909b9e644f8fa9da5edf55e27
sergeijakovlev <sergei.jakovlev.10@gmail.com>
2014-04-23 14:18:12 +0300

    [tests] Expand MyPlan test.

--- selenium_tests/tests/ee/ut/mt/webphp/tests/MyPlanFirefoxTest.java
+++ selenium_tests/tests/ee/ut/mt/webphp/tests/MyPlanFirefoxTest.java
@@ -19,17 +19,87 @@ public class MyPlanFirefoxTest extends TestCase {
 		driver = new FirefoxDriver();
 		baseUrl = "http://regix.dev/";
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		addUsers();
+		addEvents();
 	}
-
-	@Test
-	public void testMyPlan() throws Exception {
+	
+	public void addUsers() throws Exception {
 		driver.get(baseUrl + "login/logout");
-		driver.get(baseUrl + "login/myplan");
-		driver.findElement(By.id("login")).clear();
-		driver.findElement(By.id("login")).sendKeys("tw1");
-		driver.findElement(By.id("password")).clear();
-		driver.findElement(By.id("password")).sendKeys("tw1");
-		driver.findElement(By.name("submit")).click();
+		
+		// Register users
+		driver.get(baseUrl + "reg");
+	    driver.findElement(By.id("name")).clear();
+	    driver.findElement(By.id("name")).sendKeys("testownplan1");
+	    driver.findElement(By.id("username")).clear();
+	    driver.findElement(By.id("username")).sendKeys("testownplan1");
+	    driver.findElement(By.id("password")).clear();
+	    driver.findElement(By.id("password")).sendKeys("testownplan1");
+	    driver.findElement(By.id("repassword")).clear();
+	    driver.findElement(By.id("repassword")).sendKeys("testownplan1");
+	    driver.findElement(By.id("email")).clear();
+	    driver.findElement(By.id("email")).sendKeys("testownplan1");
+	    driver.findElement(By.name("submit")).click();
+	    
+ 		driver.get(baseUrl + "reg");
+ 	    driver.findElement(By.id("name")).clear();
+ 	    driver.findElement(By.id("name")).sendKeys("testownplan2");
+ 	    driver.findElement(By.id("username")).clear();
+ 	    driver.findElement(By.id("username")).sendKeys("testownplan2");
+ 	    driver.findElement(By.id("password")).clear();
+ 	    driver.findElement(By.id("password")).sendKeys("testownplan2");
+ 	    driver.findElement(By.id("repassword")).clear();
+ 	    driver.findElement(By.id("repassword")).sendKeys("testownplan2");
+ 	    driver.findElement(By.id("email")).clear();
+ 	    driver.findElement(By.id("email")).sendKeys("testownplan2");
+ 	    driver.findElement(By.name("submit")).click();
+	    
+ 	    // Login as admin
+	    driver.get(baseUrl + "login");
+	    driver.findElement(By.id("login")).clear();
+	    driver.findElement(By.id("login")).sendKeys("test");
+	    driver.findElement(By.id("password")).clear();
+	    driver.findElement(By.id("password")).sendKeys("test_password");
+	    driver.findElement(By.name("submit")).click();
+	    
+	    // Add group
+	    driver.get(baseUrl + "groups/add/testownplan");
+	    
+	    // Add group permissions
+	    driver.get(baseUrl + "groups/add_permission/testownplan/myplan_own_plan");
+	    
+	    // Add user to group
+	    driver.get(baseUrl + "groups/add_user/testownplan");
+	    driver.findElement(By.xpath("//td[contains(text(), \"testownplan1\")]/../td/a")).click();
+	}
+	
+	public void removeUsers() throws Exception {
+	    
+ 	    // Login as admin
+	    driver.get(baseUrl + "login");
+	    driver.findElement(By.id("login")).clear();
+	    driver.findElement(By.id("login")).sendKeys("test");
+	    driver.findElement(By.id("password")).clear();
+	    driver.findElement(By.id("password")).sendKeys("test_password");
+	    driver.findElement(By.name("submit")).click();
+	    
+	    // Remove users
+	    driver.get(baseUrl + "uac");
+	    driver.findElement(By.xpath("//td/div[contains(text(), \"testownplan1\")]/../../td/div/a")).click();
+	    driver.get(baseUrl + "uac");
+	    driver.findElement(By.xpath("//td/div[contains(text(), \"testownplan2\")]/../../td/div/a")).click();
+	    
+	    // Remove group
+	    driver.get(baseUrl + "groups/delete/testownplan");
+	}
+	
+	public void addEvents() throws Exception {
+	}
+	
+	public void removeEvents() throws Exception {
+	}
+	
+	// Generic complex asserts.
+	public void assertMyPlanUI() throws Exception {
 		assertTrue(isElementPresent(By.id("navigation_home")));
 		assertTrue(isElementPresent(By.id("navigation_prev")));
 		assertTrue(isElementPresent(By.id("navigation_now")));
@@ -44,11 +114,39 @@ public class MyPlanFirefoxTest extends TestCase {
 		assertTrue(isElementPresent(By.id("time_slot_7")));
 		assertTrue(isElementPresent(By.id("time_slot_8")));
 		assertTrue(isElementPresent(By.id("time_slot_9")));
-		driver.get(baseUrl + "login/logout");
+	}
+	
+	@Test
+	public void testMyPlanLogin() throws Exception {
+		// Only users with permission myplan_own_plan can access their own plan.
+		
+		// Login as testownplan1 (has access)
+	    driver.get(baseUrl + "login");
+	    driver.findElement(By.id("login")).clear();
+	    driver.findElement(By.id("login")).sendKeys("testownplan1");
+	    driver.findElement(By.id("password")).clear();
+	    driver.findElement(By.id("password")).sendKeys("testownplan1");
+	    driver.findElement(By.name("submit")).click();
+	    
+		driver.get(baseUrl + "myplan");
+		assertMyPlanUI();
+		
+		// Login as testownplan2 (no access)
+	    driver.get(baseUrl + "login");
+	    driver.findElement(By.id("login")).clear();
+	    driver.findElement(By.id("login")).sendKeys("testownplan2");
+	    driver.findElement(By.id("password")).clear();
+	    driver.findElement(By.id("password")).sendKeys("testownplan2");
+	    driver.findElement(By.name("submit")).click();
+	    
+		driver.get(baseUrl + "myplan");
+		assertTrue(isElementPresent(By.className("error_message_block")));
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		removeEvents();
+		removeUsers();
 		driver.quit();
 		String verificationErrorString = verificationErrors.toString();
 		if (!"".equals(verificationErrorString)) {
